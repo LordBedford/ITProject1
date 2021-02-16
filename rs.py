8bf412b8fcb6aa91ac0501b6573b9a736a061da9
LordBedford <acater@gmail.com>
2021-02-15 20:27:59 -0500

    Changes to rs.py

--- rs.py
+++ rs.py
@@ -18,28 +18,31 @@ host = socket.gethostname()
 print("[S]: Server host name is {}".format(host))
 localhost_ip = (socket.gethostbyname(host))
 print("[S]: Server IP address is {}".format(localhost_ip))
-csockid, addr = ss.accept()
-data = csockid.recv(1000)
-data = data.decode("UTF-8","strict")
-serverList = {
-  "qtsdatacenter.aws.com": "128.64.3.2 A",
-  "mx.rutgers.edu": "192.64.4.2 A",
-  "kill.cs.rutgers.edu": "182.48.3.2 A",
-  "mx.rutgers.edu": "192.64.4.2 A",
-  "www.ibm.com": "192.64.4.2 A",
-  "www.google.com": "8.6.4.2 A"
-}
-print("[S]: The requested domain is: ", data)
-if data in serverList:
-    csockid.send(serverList[data].encode('utf-8'))
-else:
-    csockid.send("localhost - NS".encode('utf-8'))
-print("[S]: Got a connection request from a client at {}".format(addr))
+while(True):
+    csockid, addr = ss.accept()
+    data = csockid.recv(1000)
+    if not data:
+        break
+    data = data.decode("UTF-8","strict")
+    serverList = {
+      "qtsdatacenter.aws.com": "128.64.3.2 A",
+      "mx.rutgers.edu": "192.64.4.2 A",
+      "kill.cs.rutgers.edu": "182.48.3.2 A",
+      "mx.rutgers.edu": "192.64.4.2 A",
+      "www.ibm.com": "192.64.4.2 A",
+      "www.google.com": "8.6.4.2 A"
+    }
+    print("[S]: The requested domain is: ", data)
+    if data in serverList:
+        csockid.send(serverList[data].encode('utf-8'))
+    else:
+        csockid.send("localhost - NS".encode('utf-8'))
+    print("[S]: Got a connection request from a client at {}".format(addr))
 
-# send a intro message to the client.  
-msg = "Welcome to CS 352!"
-csockid.send(msg.encode('utf-8'))
+    # send a intro message to the client.
+    msg = "Welcome to CS 352!"
+    csockid.send(msg.encode('utf-8'))
 
-# Close the server socket
-ss.close()
+    # Close the server socket
+    ss.close()
 exit()
