fbe0b238b7ca67a22577468ce76f5d80cf7e4c98
Jincredible <jink615@gmail.com>
2021-02-16 14:45:04 -0500

    modified client server to create a new connection per request

--- client.py
+++ client.py
@@ -1,11 +1,7 @@
 import socket
 import time
 
-def client():
-    #open input folder and store lines in sites list
-    with open('PROJI-HNS.txt') as f:
-        sites = f.readlines()
-
+def client(request):
     try:
         cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[C]: Client socket created")
@@ -21,16 +17,32 @@ def client():
     server_binding = (localhost_addr, port)
     cs.connect(server_binding)
 
-    #send 2 strings to server
-    cs.send("www.google.com".encode('utf-8'))
+    #encode request and send it to root server
+    cs.send(request.encode('utf-8'))
+
+    #recieve data from server
     data_from_server = cs.recv(100)
     print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
-    cs.send("www.ibm.com".encode('utf-8'))
-    data_from_server = cs.recv(100)
-    print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+
 
     # close the client socket
     cs.close()
-    exit()
+    return data_from_server
 
-client()
+def client_driver():
+
+    # open input folder and store lines in sites list
+    with open('PROJI-HNS.txt') as f:
+        sites = f.readlines()
+
+    #List of thruples to store responses
+    responses = list()
+
+    #create connection and send request to RS for each website
+    for site in sites:
+        response = client(site)
+        responses.append( (site, response) )
+
+    print(responses)
+
+client_driver()
