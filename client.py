7506a1752322571975e0891bbc1f2d0ceae3f266
Jincredible <jink615@gmail.com>
2021-02-19 13:24:56 -0500

    added ability to write to file

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 import sys
 
-def client(request,port):
+def client(request, port, host ):
     try:
         cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[C]: Client socket created")
@@ -9,39 +9,50 @@ def client(request,port):
         print('socket open error: {} \n'.format(err))
         exit()
 
-    ts_port = int(sys.argv[2])
+    #define the ts_port as the 4th arg
+    ts_port = int(sys.argv[3])
 
-    # Define the port on which you want to connect to the server
-    localhost_addr = socket.gethostbyname(socket.gethostname())
+    # get address of the host
+    addr = socket.gethostbyname(host)
 
-    # connect to the server on local machine
-    server_binding = (localhost_addr, port)
+    # connect to the server using its address
+    server_binding = (addr, port)
     cs.connect(server_binding)
 
     #encode request and send it to root server
     cs.send(request.encode('utf-8'))
 
-    #recieve data from server
-    data_from_server = cs.recv(100)
-    print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+    #recieve response, decode it
+    response = cs.recv(100).decode('utf-8')
+
+    #if the request is not found, return ERROR
+    if response == "- Error:HOST NOT FOUND":
+        return response
+
+    #print("[C]: Data received from server: {}".format(data_from_server))
+
+    data_from_server = response.split()
 
     #if the rs server respons with "localhost - NS" call client again on the
-    if data_from_server.decode('utf-8') == "localhost - NS":
-        data_from_server = client( request, ts_port)
-
+    if data_from_server[-1] == "NS":
+        response = client( request, ts_port, host)
 
     # close the client socket
     cs.close()
-    return data_from_server
+    return response
 
 def client_driver():
 
-    rs_port = int(sys.argv[1])
+    rs_host_name = sys.argv[1]
+    rs_port = int(sys.argv[2])
+
     print(rs_port)
 
     # open input folder and store lines in sites list
     with open('PROJI-HNS.txt') as f:
         sites = f.readlines()
+
+    outF = open("RESOLVED1.txt", "w")
 
     #List of thruples to store responses
     responses = list()
@@ -49,11 +60,12 @@ def client_driver():
     #create connection and send request to RS for each website
     for site in sites:
         site = site.rstrip('\n')
-        print(site)
-        response = client(site,rs_port)
-        responses.append( (site, response) )
-
-    print( responses )
+        response = client(site,rs_port, rs_host_name)
+        outF.write(site)
+        outF.write(" ")
+        outF.write(response)
+        outF.write("\n")
+    outF.close()
 
 
 client_driver()
